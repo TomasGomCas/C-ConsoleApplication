9c57847e504bfe175efc3fef8aa852d8fbc0758e
TomasGomCas <38647610+TomasGomCas@users.noreply.github.com>
2019-09-05 21:55:39 +0200

    Create class Post and Muro

---
+++ Proyect/CSharp-ConsoleApplication/Classes/Muro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_ConsoleApplication.Classes
+{
+    class Muro
+    {
+        // ATRIBUTES
+        private List<Post> posts;
+
+        // CONSTRUCTOR
+        public Muro(List<Post> posts)
+        {
+            this.posts = posts;
+        }
+
+        // METHODS
+
+        // INHERITED METHODS
+        public override string ToString()
+        {
+            string text = "";
+            foreach (Post post in this.posts)
+            {
+                text = text + " // USER: " + post.User + " // TEXT: " + post.Text + "\n\n";
+            }
+
+            return text;
+        }
+
+        // GETTERS AND SETTERS
+        internal List<Post> Posts { get => posts; set => posts = value; }
+
+    } // END
+}
---
+++ Proyect/CSharp-ConsoleApplication/Classes/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_ConsoleApplication.Classes
+{
+    class Post
+    {
+        // ATRIBUTES
+        private string user;
+        private string text;
+
+        // CONSTRUCTOR
+        public Post(string user, string text)
+        {
+            this.user = user;
+            this.text = text;
+        }
+
+        // METHODS
+
+        // GETTERS AND SETTERS
+        public string User { get => user; set => user = value; }
+        public string Text { get => text; set => text = value; }
+    }
+}
